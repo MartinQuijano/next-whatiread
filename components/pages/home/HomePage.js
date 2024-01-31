58e18de17e3a1ba0b6310f481d92ae331f2642a9
MartinQuijano <mnquijanoalt@gmail.com>
2024-01-31 09:09:48 -0300

    Add a hero section when user is not logged in

--- components/pages/home/HomePage.js
+++ components/pages/home/HomePage.js
@@ -13,9 +13,11 @@ import moment from "moment/moment";
 import styles from "./homepage.module.css";
 import { usePagination } from "@/hooks/usePagination";
 import { add } from "@/services/book";
-import Pagination from "@/components/pagination/pagination";
+import Pagination from "@/components/pagination/Pagination";
+import Image from "next/image";
 
 export default function HomePage() {
+  const [token, setToken] = useState();
   const [books, setBooks] = useState([]);
 
   const [book, setBook] = useState();
@@ -31,6 +33,10 @@ export default function HomePage() {
   } = useForm({
     resolver: yupResolver(bookSchema),
   });
+
+  useEffect(() => {
+    setToken(Cookies.get("token"));
+  }, []);
 
   useEffect(() => {
     const token = Cookies.get("token");
@@ -58,33 +64,47 @@ export default function HomePage() {
 
   return (
     <main className={styles.main}>
-      <section className={styles.options}>
-        <form className={styles.add_book_form} onSubmit={() => {}}>
-          <input
-            className={styles.input}
-            type="text"
-            name="title"
-            value={book || ""}
-            placeholder="Book title"
-            {...registerBook("title", { onChange: (e) => setBook(e.target.value) })}
-            autoFocus
-          />
-          <DatePicker className={styles.date_picker} name="title" selected={date} onChange={(date) => setDate(date)} />
-          <button className={"button"} type="submit" onClick={handleSubmitBook(addNewBook)}>
-            Add
-          </button>
-        </form>
-        {errorsBook.title && <span className={styles.error}>{errorsBook.title.message}</span>}
-      </section>
-      <section className={styles.books_container}>
-        <div className={styles.books}>
-          {books.length > 0 &&
-            books.map((book) => (
-              <BookCard key={book.id} title={book.title} date={book.date === null ? "xx/xx/xxxx" : moment(book.date).format("DD/MM/YYYY")} />
-            ))}
-        </div>
-        <Pagination currentPage={currentPage} totalPages={totalPages} prevPage={prevPage} nextPage={nextPage} />
-      </section>
+      {!token ? (
+        <section className={styles.hero}>
+          <div className={styles.information}>
+            <h1 className={styles.title}>TRACK YOUR READS, MARK YOUR MOMENTS</h1>
+            <h3 className={styles.subtitle}>Centralize your reading journey: all your book records in one place</h3>
+          </div>
+          <div className={styles.logo_container}>
+            <Image className={styles.image} src="/whatiread_hero.png" alt="main_logo" width={0} height={0} sizes="100vw" />
+          </div>
+        </section>
+      ) : (
+        <>
+          <section className={styles.options}>
+            <form className={styles.add_book_form} onSubmit={() => {}}>
+              <input
+                className={styles.input}
+                type="text"
+                name="title"
+                value={book || ""}
+                placeholder="Book title"
+                {...registerBook("title", { onChange: (e) => setBook(e.target.value) })}
+                autoFocus
+              />
+              <DatePicker className={styles.date_picker} name="title" selected={date} onChange={(date) => setDate(date)} />
+              <button className={"button"} type="submit" onClick={handleSubmitBook(addNewBook)}>
+                Add
+              </button>
+            </form>
+            {errorsBook.title && <span className={styles.error}>{errorsBook.title.message}</span>}
+          </section>
+          <section className={styles.books_container}>
+            <div className={styles.books}>
+              {books.length > 0 &&
+                books.map((book) => (
+                  <BookCard key={book.id} title={book.title} date={book.date === null ? "xx/xx/xxxx" : moment(book.date).format("DD/MM/YYYY")} />
+                ))}
+            </div>
+            <Pagination currentPage={currentPage} totalPages={totalPages} prevPage={prevPage} nextPage={nextPage} />
+          </section>
+        </>
+      )}
     </main>
   );
 }
