be05e88c3eca7ea45c4521e5747974ad3c43f2eb
MartinQuijano <mnquijanoalt@gmail.com>
2024-02-03 15:26:13 -0300

    Change the color of the page numbers

--- components/pagination/Pagination.js
+++ components/pagination/Pagination.js
@@ -7,7 +7,9 @@ export default function Pagination({ currentPage, totalPages, prevPage, nextPage
       <button className={`button`} onClick={prevPage} disabled={currentPage === 0}>
         Prev
       </button>
-      {totalPages > 0 && currentPage + 1} - {totalPages > 0 && totalPages}
+      <span className={styles.pages}>
+        {totalPages > 0 && currentPage + 1} - {totalPages > 0 && totalPages}
+      </span>
       <button className={`button`} onClick={nextPage} disabled={currentPage === totalPages - 1}>
         Next
       </button>
