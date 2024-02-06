02a374a4c32ffc8e9cbc3d145f03353a5a6de5f6
MartinQuijano <mnquijanoalt@gmail.com>
2024-02-06 11:40:40 -0300

    Add the SearchForm component to the Filters component

--- components/filters/Filters.js
+++ components/filters/Filters.js
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./filters.module.css";
 import Dropdown from "../dropdown/Dropdown";
+import SearchForm from "../search-form/SearchForm";
 
-export default function Filters({ sort, handleSortChange, handleOrderChange }) {
+export default function Filters({ sort, handleSortChange, handleOrderChange, search, handleSearchChange, handleSearch }) {
   const options = [
     { name: "Title", filter: "book.title" },
     { name: "Date", filter: "date" },
   ];
   return (
     <div className={styles.container}>
+      <SearchForm search={search} handleSearchChange={handleSearchChange} handleSearch={handleSearch} />
       <Dropdown title={sort} options={options} handleSelection={handleSortChange} handleSameSelection={handleOrderChange} />
     </div>
   );
